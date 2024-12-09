d436c349761cab55cf1f35481f2924877662611d
jonuoha60 <80774239+jonuoha60@users.noreply.github.com>
2024-12-09 02:23:30 -0500

    Add files via upload

---
+++ src/Contacts.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Contacts {
+    public static void main(String[] args) {
+        FrameClass frame = new FrameClass();
+        frame.setSize(460, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
